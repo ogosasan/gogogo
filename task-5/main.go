55880f3b9aba3cd44f5929513b5e8142871b5b52
ogosasan <114873628+ogosasan@users.noreply.github.com>
2023-11-28 13:08:34 +0300

    Update main.go

--- task-5/main.go
+++ task-5/main.go
@@ -13,11 +13,8 @@ func main() {
 	text = strings.TrimSpace(text)
 	var a []string
 	a = strings.Split(text, "")
-	var N = "one"
 	for i := 0; i < len(a); i++ {
-		if a[i] == "1" {
-			a[i] = N
-		}
+		a[i] = strings.ReplaceAll(a[i], "1", "one")
 	}
 	for i := 0; i < len(a); i++ {
 		fmt.Print(a[i])
