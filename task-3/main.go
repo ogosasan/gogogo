3ef379564f3f386e86005740aa3520ed7d7389b8
ogosasan <114873628+ogosasan@users.noreply.github.com>
2023-11-28 13:02:22 +0300

    Update main.go

--- task-3/main.go
+++ task-3/main.go
@@ -1,25 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 )
 
 func main() {
 	var N int
 	fmt.Scanf("%d\r", &N)
-	reader := bufio.NewReader(os.Stdin)
-	text, _ := reader.ReadString('\n')
-	text = strings.TrimSpace(text)
-	var a []string
-	a = strings.Split(text, " ")
-	var last = a[N-1]
-	for i := N - 1; i > 0; i-- {
-		a[i] = a[i-1]
+	b := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Scan(&b[i])
 	}
-	a[0] = last
-	fmt.Print(a)
+	var last = b[N-1]
+	for i := N - 1; i > 0; i-- {
+		b[i] = b[i-1]
+	}
+	b[0] = last
+	fmt.Print(b)
 
 }
